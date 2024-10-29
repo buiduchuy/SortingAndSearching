6d9abe405f2dfce2404af42d230300616e9bd829
buiduchuy <huyxhhz@gmail.com>
2024-10-29 10:36:38 +0700

    Add files via upload

--- SearchingAndSorting/src/InsertionSort.java
+++ SearchingAndSorting/src/InsertionSort.java
@@ -1,13 +1,14 @@
 public class InsertionSort {
-    public static void InsertionSort(int[] arr){
+    public static void InsertionSort(int[] arr) {
         int n = arr.length;
-        for(int i =0;i<n;i++){
+        for (int i = 1; i < n; i++) {
             int temp = arr[i];
-            int j = i-1;
-            while (j>=0 && arr[j] <temp){
-                arr[j+1] = arr[j];
-                j =j-1;
-            }arr[j+1] = temp;
+            int j = i - 1;
+            while (j >= 0 && arr[j] > temp) {
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = temp;
         }
     }
 
